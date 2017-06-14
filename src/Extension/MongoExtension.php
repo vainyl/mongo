bb518c0f7401c844f166d4150deb8261e04cba67
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 17:10:45 +0300

    Updated php version

--- src/Extension/MongoExtension.php
+++ src/Extension/MongoExtension.php
@@ -21,4 +21,11 @@ use Vainyl\Core\Extension\AbstractExtension;
  */
 class MongoExtension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
+    public function getCompilerPasses(): array
+    {
+        return [];
+    }
 }
